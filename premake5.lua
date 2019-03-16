e80e49d8338b790df2f3030255fca90234e2169d
unknown <abdullrohman514@gmail.com>
2019-03-15 21:29:05 -0700

    remove windows precompiled headers for glfw and replace with glfw source code as a submodule

--- premake5.lua
+++ premake5.lua
@@ -24,13 +24,13 @@ project "Core"
 
     defines { "IMGUI_IMPL_OPENGL_LOADER_GLAD", "IMGUI_HAS_DOCK" }
 
-    links { "OpenGL32.lib" }
+    links { "OpenGL32.lib", "glfw" }
 
-    filter "platforms:x64"
-      links { "lib/glfw3_64.lib" }
+    --filter "platforms:x64"
+    --  links { "lib/glfw3_64.lib" }
 
-    filter "platforms:x86"
-      links { "lib/glfw3_32.lib" }
+    --filter "platforms:x86"
+    --  links { "lib/glfw3_32.lib" }
 
     filter "configurations:Debug"
       symbols "On"
@@ -42,3 +42,37 @@ project "Core"
 
     filter "system:Windows"
       systemversion ("latest")
+
+
+      project "glfw"
+      kind ("StaticLib")
+      files {
+        "glfw/src/init.c",
+        "glfw/src/input.c",
+        "glfw/src/window.c",
+        "glfw/src/internal.h",
+        "glfw/src/monitor.c",
+        "glfw/src/vulkan.c",
+        "glfw/src/wgl_context.c",
+        "glfw/src/wgl_context.h",
+        "glfw/src/egl_context.c",
+        "glfw/src/egl_context.h",
+        "glfw/src/osmesa_context.c",
+        "glfw/src/osmesa_context.h",
+        "glfw/src/context.c"
+      }
+  
+      filter "system:Windows"
+       files {
+         "glfw/src/win32_platform.h",
+         "glfw/src/win32_init.c",
+         "glfw/src/win32_monitor.c",
+         "glfw/src/win32_thread.c",
+         "glfw/src/win32_time.c",
+         "glfw/src/win32_joystick.c",
+         "glfw/src/win32_joystick.h",
+         "glfw/src/win32_window.c"
+       }
+       systemversion ("latest")
+       defines { "_GLFW_WIN32" }
+  
