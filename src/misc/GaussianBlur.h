3081d111881e5233a23739876b54cb31fe16cee9
abdullrohman514 <abdullrohman514@gmail.com>
2019-07-28 20:38:35 +0400

    remove old blur method declarations

--- src/misc/GaussianBlur.h
+++ src/misc/GaussianBlur.h
@@ -7,10 +7,7 @@ namespace ALZ {
 	class GaussianBlur {
 	public:
 		static void Init();
-		static void Blur(RenderSurface& surface, const float& scale, const float& blurScale, const float& sigma);
 		static void Blur(RenderSurface& surface, float radius);
-		
-		static float GaussianDistribution(float x, float sigma);
 
 	private:
 		static ShaderProgram* Hblur5x5;
