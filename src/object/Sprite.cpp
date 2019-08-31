70e12ed9fbc1b6b21a0fbda2453acec33465ab0a
abdullrohman514 <abdullrohman514@gmail.com>
2019-08-31 15:39:58 +0400

    add default texture for sprites
    add GrayScaleToRGB function in image class because OpenGL does not support grayscale rendering unless we specify that in the shader that we are using the red channel for all 3 RGB channels, and used that function when loading the default sprite.\n Note: this function is not called by default when loading Grayscale images because we might want to load one channel textures for purposes other than rendering it directly to the screen

--- src/object/Sprite.cpp
+++ src/object/Sprite.cpp
@@ -29,6 +29,15 @@ namespace ALZ {
 		m_VertexBuffer->SetLayout({ ShaderVariableType::Vec2, ShaderVariableType::Vec2 });
 
 		m_ShaderProgram = Renderer::CreateShaderProgram("shaders/Sprite.vert", "shaders/Sprite.frag");
+
+		m_Texture = Renderer::CreateTexture();
+
+		Image img = Image::LoadFromFile("res/CheckerBoard.png");
+
+		Image::GrayScaleToRGB(img);
+
+		m_Texture->SetImage(img);
+
 	}
 
 	void Sprite::Update(const float& deltaTime)
@@ -47,6 +56,12 @@ namespace ALZ {
 		ImGui::PushID(this);
 
 		ImGui::Begin((m_Name + "##" + std::to_string(ImGui::GetID(this))).c_str(), &EditorOpen, ImGuiWindowFlags_NoSavedSettings);
+
+		ImGui::Text("Texture: ");
+		ImGui::SameLine();
+		ImGui::Image((ImTextureID)m_Texture->GetRendererID(), ImVec2(100, 100), ImVec2(0, 0), ImVec2(1, 1), ImVec4(1, 1, 1, 1), ImVec4(1, 1, 1, 1));
+
+		ImGui::Spacing();
 
 		ImGui::Text("Position: ");
 		ImGui::SameLine();
--- src/renderer/opengl/OpenGLTexture.cpp
+++ src/renderer/opengl/OpenGLTexture.cpp
@@ -21,9 +21,11 @@ namespace ALZ {
 			Bind();
 
 			if (image.m_Comp == 3)
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.m_Width, image.m_Height, 0, GL_RGB, GL_UNSIGNED_BYTE, image.m_Data);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, image.m_Width, image.m_Height, 0, GL_RGB, GL_UNSIGNED_BYTE, image.m_Data);
 			else if (image.m_Comp == 4)
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.m_Width, image.m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.m_Data);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image.m_Width, image.m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.m_Data);
+			else if(image.m_Comp == 1)
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, image.m_Width, image.m_Height, 0, GL_RED, GL_UNSIGNED_BYTE, image.m_Data);
 			else
 				assert(false);
 
